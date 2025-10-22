b97debdfab1ccfb44dee5472c6a50e5f0652603f
Top Code <162437900+Coderad32@users.noreply.github.com>
2025-10-21 18:48:03 -0600

    Initialize Hardhat project with dependencies
    Set up a new Hardhat project for Ethereum development.

---
+++ blockchain/hardhat.sh
@@ -0,0 +1,4 @@
+mkdir mytoken && cd mytoken
+npm init -y
+npm install --save-dev hardhat @nomiclabs/hardhat-ethers ethers chai mocha @openzeppelin/contracts
+npx hardhat    # choose "Create an empty hardhat.config.js" or "Basic sample project" (either is fine)
